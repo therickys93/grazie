56d5b0f6bc7620c89375cb7f97682c63d7a69ec7
Riccardo Crippa <therickys93@gmail.com>
2017-04-03 21:57:56 +0200

    aggiunto docker e prima implementazione

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
 <?php 
-	echo "Grazie mille"; 
+	$images = [
+		"http://img.topimmagini.com/to/grazie/grazie_001.jpg", 
+		"http://www.rimborsabile.it/wp-content/uploads/2015/01/grazie.jpg", 
+		"http://www.aleslic.it/wp-content/uploads/grazie.png", 
+		"http://www.profumeriesbraccia.com/wp-content/uploads/2015/12/grazie.gif",
+		"http://aforisticamente.com/wp-content/uploads/2015/10/Frasi_per_dire_grazie_a.jpg"
+	];
+	$index = rand() % count($images);
+	$image = $images[$index]; 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Grazie</title>
+	<style type="text/css">
+		body {
+			width: 100%;
+			height: 100%;
+		}
+
+		.container {
+			width: 80%;
+			margin: 0 auto;
+		}
+
+		.image {
+			width: 100%;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<img class="image" src="<?= $image ?>" alt="">
+	</div>
+</body>
+</html>
